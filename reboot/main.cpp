27400cdbdd1d52d93bf1dd3b29c83632d76bc2d1
Егор Шульга <egorsulga0@gmail.com>
2024-02-15 21:34:20 +0300

    1

--- reboot/main.cpp
+++ reboot/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void FillRand(int arr[], int n, int x, int minRand = 0, int maxRand = 100);
+void FillRand(int arr[], int n, int x);
 void Print(int arr[], int n, int x);
 
 void main()
@@ -16,21 +16,7 @@ void main()
 	{1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 }
 	};*/	// двумерный массив типа int 
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < x; j++)
-		{
-			arr[i][j] = i + j;
-		}
-	}
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < x; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-	}
 
 }
 
