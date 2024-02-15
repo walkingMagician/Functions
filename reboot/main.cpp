d522d161cbd2eb150a14a75543bfb839a4fa7acb
Егор Шульга <egorsulga0@gmail.com>
2024-02-15 21:30:25 +0300

    0

--- reboot/main.cpp
+++ reboot/main.cpp
@@ -2,6 +2,9 @@
 #include <iomanip>
 using namespace std;
 
+void FillRand(int arr[], int n, int x, int minRand = 0, int maxRand = 100);
+void Print(int arr[], int n, int x);
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -14,23 +17,5 @@ void main()
 	{1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 }
 	};*/	// двумерный массив типа int 
 
-	for (int i = 0; i < n; i++)		
-	{
-		for (int j = 0; j < x; j++)
-		{
-			arr[i][j] = i + j;
-		}
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < x; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-
-
-
 }
+
