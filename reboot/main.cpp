0cd502aa117eb340189b817b40e8d75a038302ef
Егор Шульга <egorsulga0@gmail.com>
2024-02-15 23:59:29 +0300

    инациализация 2D arr и 2 простые функции

--- reboot/main.cpp
+++ reboot/main.cpp
@@ -1,22 +1,50 @@
 #include<iostream>
 using namespace std;
 
-void FillRand(int arr[], int n, int x);
-void Print(int arr[], int n, int x);
+void FillRand(int** arr, int n, int x);
+void Print(int** arr, int n, int x);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
-	const int n = 5, x = 5;
-	int arr[n][x];
-	/*{ 
-	{1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 },
-	{1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 },
-	{1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 }
-	};*/	// двумерный массив типа int 
+	int n, x;
+	cout << "n = "; cin >> n;
+	cout << "x = "; cin >> x;
+
+	int** arr = new int* [n];
+	for (int i = 0; i < x; ++i)
+	{
+		arr[i] = new int[x];
+	}
 
 
+	FillRand(arr, n, x);
+	Print(arr, n, x);
 
 }
 
+void FillRand(int** arr, int n, int x)
+{
+	int minRand = 0;
+	int maxRand = 100;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			arr[i][j] = minRand + rand() % (maxRand - minRand);
+		}
+	}
+}
+
+void Print(int** arr, int n, int x)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
